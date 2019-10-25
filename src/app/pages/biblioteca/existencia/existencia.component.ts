99bc012874f3e25d3aa53a88b308d2aa0646cc5a
Caterin Thomas <katerinthomas_1996@hotmail.com>
2019-10-24 19:39:09 -0500

    FORMULARIO REACTIVO CATERIN THOMAS

--- src/app/pages/biblioteca/existencia/existencia.component.ts
+++ src/app/pages/biblioteca/existencia/existencia.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-existencia',
@@ -6,10 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./existencia.component.css']
 })
 export class ExistenciaComponent implements OnInit {
-
-  constructor() { }
+public autoForm:FormGroup;
+  constructor(protected fb:FormBuilder) { }
 
   ngOnInit() {
+  }
+  createForm(){
+    this.autoForm=this.fb.group({
+  codigo:'',
+  nombre:'',
+  fechaNac:''
+    });
   }
 
 }
--- src/app/pages/biblioteca/existencia/existencia.module.ts
+++ src/app/pages/biblioteca/existencia/existencia.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import {FormsModule,ReactiveFormsModule} from '@angular/forms'
 import { ExistenciaRoutingModule } from './existencia-routing.module';
 import { ExistenciaFormComponent } from './existencia-form/existencia-form.component';
 import { ExistenciaTableComponent } from './existencia-table/existencia-table.component';
@@ -15,7 +15,9 @@ import { ExistenciaComponent } from './existencia.component';
   ],
   imports: [
     CommonModule,
-    ExistenciaRoutingModule
+    ExistenciaRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ]
 })
 export class ExistenciaModule { }
