94d814bab04a336ef4f95609b7c3fba0d8024903
Katerin thomas villega <katerinthomas_1996@hotmail.com>
2019-11-07 17:17:53 -0500

    formulario reactive autores

--- src/app/pages/biblioteca/autores/autores-form/autores-form.component.ts
+++ src/app/pages/biblioteca/autores/autores-form/autores-form.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 @Component({
   selector: 'app-autores-form',
   templateUrl: './autores-form.component.html',
   styleUrls: ['./autores-form.component.css']
 })
-export class AutoresFormComponent implements OnInit {
-
-  constructor() { }
-
+export class AutorFormComponent implements OnInit {
+  public autorFormulario: FormGroup;
+  public titulo="Autores";
+  constructor(protected fb:FormBuilder) { }
   ngOnInit() {
+  }
+  createForm(){
+    this.autorFormulario = this.fb.group({
+      code:['',Validators.required],
+      name:['',Validators.required],
+      lastname:['',Validators.required],
+      bd_year :['',Validators.required],
+      death_year :['',Validators.required],
+      bd_place :['',Validators.required]
+    })
   }
 
 }
--- src/app/pages/biblioteca/autores/autores.module.ts
+++ src/app/pages/biblioteca/autores/autores.module.ts
@@ -3,14 +3,14 @@ import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AutoresRoutingModule } from './autores-routing.module';
-import { AutoresFormComponent } from './autores-form/autores-form.component';
+import { AutorFormComponent } from './autores-form/autores-form.component';
 import { AutoresTableComponent } from './autores-table/autores-table.component';
 import { AutoresComponent } from './autores.component';
 
 
 @NgModule({
   declarations: [
-    AutoresFormComponent, 
+    AutorFormComponent, 
     AutoresTableComponent, 
     AutoresComponent
   ],
