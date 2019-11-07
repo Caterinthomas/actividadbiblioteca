c84e077b2e19143ef82a3d99dafc927eb65627c9
Katerin thomas villega <katerinthomas_1996@hotmail.com>
2019-11-07 17:22:47 -0500

    formulario reactive editorial

--- src/app/pages/biblioteca/editorial/editorial-form/editorial-form.component.ts
+++ src/app/pages/biblioteca/editorial/editorial-form/editorial-form.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 @Component({
   selector: 'app-editorial-form',
   templateUrl: './editorial-form.component.html',
   styleUrls: ['./editorial-form.component.css']
 })
 export class EditorialFormComponent implements OnInit {
-
-  constructor() { }
-
+  public editorialFormulario : FormGroup;
+  public titulo = "Editorial";
+  constructor(protected fb:FormBuilder) { }
   ngOnInit() {
+  }
+  CreateForm(){
+    this.editorialFormulario = this.fb.group({
+      code: ['',Validators.required],
+      descripcion: ['',Validators.required],
+      country: ['',Validators.required]
+    })
   }
 
 }
