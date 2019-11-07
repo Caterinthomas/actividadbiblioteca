ba3e444005b0da4da8ee06bfbe1617a309d9cd83
Katerin thomas villega <katerinthomas_1996@hotmail.com>
2019-11-07 17:12:04 -0500

    formulario reactive areas

--- src/app/pages/biblioteca/areas/areas-form/areas-form.component.ts
+++ src/app/pages/biblioteca/areas/areas-form/areas-form.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder,FormGroup,Validators} from '@angular/forms';
 @Component({
   selector: 'app-areas-form',
   templateUrl: './areas-form.component.html',
   styleUrls: ['./areas-form.component.css']
 })
 export class AreasFormComponent implements OnInit {
-
-  constructor() { }
-
+  public areasFormulario: FormGroup;
+  public titulo="formulario de Areas";
+   constructor(protected fb: FormBuilder ) { }
   ngOnInit() {
   }
-
-}
+  createForm(){
+    this.areasFormulario = this.fb.group({
+      code: ['', Validators.required],
+      description: ['', Validators.required]
+      }) 
+    }
+  }
